9316f8d14250a3b76e7d4bfae9d9e5f874beb427
Sienna Yazdi <ccyazdi@gmail.com>
2020-07-22 21:41:20 -0500

    add basic functionality from terminal

---
+++ MiniProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Apple on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
