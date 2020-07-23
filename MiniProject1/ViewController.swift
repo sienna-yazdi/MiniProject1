188937bb57a822e22642c8ec9e7ef78217804757
sienna-yazdi <ccyazdi@gmail.com>
2020-07-23 13:56:31 -0500

    iteration1

--- MiniProject1/ViewController.swift
+++ MiniProject1/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fact1: UILabel!
+    
+    @IBOutlet weak var fact2: UILabel!
+    @IBOutlet weak var fact3: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func buttoon(_ sender: UIButton) {
+        
+        fact1.text = "My favorite sport is basketball"
+        fact2.text = "I love writing"
+        fact3.text = "I love reading"
+          
+          
+      }
 
 
 }
